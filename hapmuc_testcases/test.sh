9e4d3a5673db7d30ee2a8e3f9346f90ee35d7766
Naoto Usuyama <usuyama.naoto@gmail.com>
2013-05-31 18:25:22 +0900

    test script

--- hapmuc_testcases/test.sh
+++ hapmuc_testcases/test.sh
@@ -2,7 +2,8 @@
 i=$1
 echo "--- test #$i ---"
 cd ~/thesis/hapmuc3
-./dindel --analysis mutationCall --tumorBamFile hapmuc_testcases/$i/tumor.sort.bam --normalBamFile hapmuc_testcases/$i/normal.sort.bam --ref /Users/usuyama/data_store/hg19_bwa-0.5.10/hg19.fasta --varFile hapmuc_testcases/$i/windows --libFile hapmuc_testcases/$i/cigar.libraries.txt --outputFile hapmuc_testcases/$i/result/mc > hapmuc_testcases/$i/result/log
+rm hapmuc_testcases/$i/result/*
+./dindel --tumorBamFile hapmuc_testcases/$i/tumor.sort.bam --normalBamFile hapmuc_testcases/$i/normal.sort.bam --ref /Users/usuyama/data_store/hg19_bwa-0.5.10/hg19.fasta --varFile hapmuc_testcases/$i/windows --libFile hapmuc_testcases/$i/cigar.libraries.txt --outputFile hapmuc_testcases/$i/result/mc > hapmuc_testcases/$i/result/log
 cat hapmuc_testcases/$i/README
 tail hapmuc_testcases/$i/windows
 tail hapmuc_testcases/$i/windows | ruby -ane 'puts $F[-2..-1].join("\t")'
